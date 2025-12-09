8567976fddd5c7e5b7b21693e1b0ebeccea04d21
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2025-12-09 08:37:59 -0300

    Routine Button
    Added routine button. None of the buttons are doing what they're supposed to yet. CSS with Tailwind

--- myjournal/src/app/layout.tsx
+++ myjournal/src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
         className={`antialiased`}
       >
--- myjournal/src/app/page.tsx
+++ myjournal/src/app/page.tsx
@@ -1,13 +1,24 @@
 'use client'
 export default function Home() {
   function navButton() {
-    console.log("Teste");
+    console.log("NavButton");
     
   }
+  function viewRoutines(){
+    console.log("ViewRoutine")
+  }
+  function createRoutine(){
+    console.log("CreateRoutine");
+    
+  }
+  let date = new Date();
   return (
     <section>
-      <div>
-        <nav><button onClick={navButton} className="navButton">≡</button></nav>
+      <div className="flex flex-row">
+        <nav><button onClick={navButton} className="navButton bg-neutral-600 w-25 h-15 rounded-sm border-1 border-neutral-200 hover:bg-neutral-700">≡</button>
+        <button onClick={viewRoutines} className="viewRoutinesButton h-15 w-50 bg-neutral-600 rounded-sm ml-10 border-1 border-neutral-200 hover:bg-neutral-700">Routine</button>
+        <button onClick={createRoutine} className="createRoutineButton h-15 w-15 bg-neutral-600 rounded-sm ml-5 border-1 border-neutral-200 hover:bg-neutral-700">+</button></nav>
+        <h1 className="text-center">{date.toLocaleString()}</h1>
       </div>
     </section>
   );
